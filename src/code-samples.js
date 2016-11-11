c18aa5105dfe8430d8a604988564aa77c4a4ede5
Scott Hill <scott289@gmail.com>
2016-11-11 10:03:07 -0800

    content

---
+++ src/code-samples.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import './code-samples.css';
+
+const CodeSamples = () => (
+    <div className="CodeSamples">
+
+      <header>Code Samples</header>
+
+      <section>
+        <header>Plants App</header>
+
+        <p>
+          A react UI for 90,000 records of <a target="_blank" href="https://plants.usda.gov/dl_all.html">USDA plant data</a>.
+          Implemented in two ways:
+        </p>
+
+        <p>
+          <span className="project">isomorphic React-Starter-Kit</span> boilerplate (<a href="https://github.com/hillscottc/plantapp-iso">github</a>
+          | <a href="https://plantapp-iso.herokuapp.com/">running</a>)
+        </p>
+
+        <p>
+          <span className="project">Create-React-App</span> boilerplate (<a href="https://github.com/hillscottc/plantapp">github</a>
+          | <a href="https://plantapp.herokuapp.com/">running</a>)
+        </p>
+
+      </section>
+
+      <section>
+        <header>Trivia App</header>
+
+        <p>
+          A single-page web app as a UI for a collection of trivia questions. Uses <a href="https://nodejs.org/">Node</a>
+          and <a href="http://expressjs.com/">Express</a>
+          to serve an api for a PostgreSQL database of 180,000+ records.
+        </p>
+
+        <p>
+          This app has been re-implemented over time, using different technologies.
+        </p>
+
+        <br/>
+
+        <p>
+          <span className="project">trivnode-react</span>...2016 with <a href="https://facebook.github.io/react/">React</a>.
+          (<a href="https://github.com/hillscottc/trivnode-react#readme">github</a>  | <a href="https://trivnode-react.herokuapp.com/">running</a>)
+        </p>
+
+        <p>
+          <span className="project">trivnode</span>...2014 with <a href="http://backbonejs.org/">Backbone.js</a> (<a href="https://github.com/hillscottc/trivnode#readme">github</a> | <a href="https://protected-lake-8296.herokuapp.com/">running</a>)
+        </p>
+
+        <p>
+          <span className="project">quest-django</span>...2013 with Python + <a href="https://www.djangoproject.com/">Django</a> (<a href="https://github.com/hillscottc/quest#readme">github</a> | <a href="https://quest-django.herokuapp.com/">running</a>)
+        </p>
+
+      </section>
+
+      <section>
+        <header>Fortune Teller App</header>
+
+        <p><span className="project">nostra</span> (<a href="https://github.com/hillscottc/nostra#readme">github</a>
+          | <a href="https://badge.fury.io/js/nostra"><img src="https://badge.fury.io/js/nostra.svg" alt="npm version" height="18" /></a>)
+        </p>
+        <p>A <a href="https://nodejs.org/">Node</a> module for generating random-ish fortune text, hosted on the <a href="https://www.npmjs.com/">npm</a>
+          package registry.</p>
+
+        <p><span className="project">nostranode-react</span>...simple UI in Node, express, and React
+          (<a href="https://github.com/hillscottc/nostranode-react#readme">github</a>)
+        </p>
+
+        <p>
+          <span className="project">nostranode</span>...simple UI with <a href="http://getbootstrap.com/">Bootstrap</a>
+          + <a href="https://jqueryui.com/">jQuery</a>
+          (<a href="https://github.com/hillscottc/nostranode#readme">github</a> | <a href="https://nostranode.herokuapp.com/">running</a>)
+        </p>
+      </section>
+
+    </div>
+);
+
+export default CodeSamples;
